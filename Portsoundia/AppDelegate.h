798d1593927bd8f3944ca13e83308bffff0954f4
Dana Silver <dsilver1221@gmail.com>
2015-07-11 00:21:38 -0400

    Initial Commit

---
+++ Portsoundia/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Portsoundia
+//
+//  Created by Dana Silver on 7/11/15.
+//  Copyright (c) 2015 Dana Silver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Portsoundia/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Portsoundia
+//
+//  Created by Dana Silver on 7/11/15.
+//  Copyright (c) 2015 Dana Silver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
